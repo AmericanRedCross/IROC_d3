4823e01e315f908a67772de39b546e06b143e688
danbjoseph <danbjoseph@gmail.com>
2013-09-05 17:08:52 -0400

    end of day at ARC office

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
 var worldcountries = [];
 var worldcapitals = [];
+var appeals = [];
+var appealsCountries = [];
 
-var svgwidth = 960,
-    svgheight = 960;
 
-var projection = d3.geo.orthographic()
-    .clipAngle(90)
-    .rotate([-5, -5])
-    .precision(.1)
-    .translate([svgwidth / 2, svgheight / 2])
-    .scale(470);
+var width = 960,
+    height = 960,
+    m = .25, // drag sensitivity
+    format = d3.format(".1f");
 
-var gilbert = d3.geo.gilbert(projection);
+// var projection = d3.geo.orthographic()
+//     .clipAngle(90)
+//     .rotate([-5, -5])
+//     .precision(.1)
+//     .translate([width / 2, height / 2])
+//     .scale(470);
 
+// var gilbert = d3.geo.gilbert(projection);
+
+// var path = d3.geo.path().projection(gilbert)
+//   .pointRadius(2);
+
+var projection = d3.geo.projection(d3.geo.hammer.raw(1.75, 2))
+    .rotate([-10, -45])
+    .scale(210);
 
 var path = d3.geo.path()
-  .projection(gilbert)
-  .pointRadius(2);
+    .projection(projection)
+    .pointRadius(2);
+
+var line = d3.svg.line()
+  .x(function(d) { return d.x; })
+  .y(function(d) { return d.y; })
+
+  .interpolate("linear");
+
 
 var graticule = d3.geo.graticule();
 
 var svg = d3.select("#map1").append("svg")
-  .attr("width", svgwidth)
-  .attr("height", svgheight);
+  .attr("width", width)
+  .attr("height", height);
 
 var countryGroup = svg.append('g').attr("id", "countries");
 var arcGroup = svg.append('g').attr("id", "arcs");
@@ -37,8 +55,31 @@ function getcountrydata(){
       dataType: 'json',
       timeout: 10000,
       success: function(json) {
-          worldcountries = json;
-          getcapitaldata();
+        worldcountries = json;
+        getappealdata();
+      },
+      error: function(e) {
+          console.log(e);
+      }
+  });
+}
+
+function getappealdata(){
+  $.ajax({
+      type: 'GET',
+      url: 'data/demoData_adm0_cut.json',
+      contentType: 'application/json',
+      dataType: 'json',
+      timeout: 10000,
+      success: function(json) {
+        appeals = json;
+        $(appeals).each(function(i, appeal){
+          country = appeal.ADM0_A3;
+          if ($.inArray(country, appealsCountries) == -1){
+            appealsCountries.push(country);
+          }
+        });
+        getcapitaldata();
       },
       error: function(e) {
           console.log(e);
@@ -54,11 +95,11 @@ function getcapitaldata(){
       dataType: 'json',
       timeout: 10000,
       success: function(json) {
-          worldcapitals = json;
-          mapIt();
+        worldcapitals = json;
+        mapIt();
       },
       error: function(e) {
-          console.log(e);
+        console.log(e);
       }
   });
 }
@@ -93,11 +134,12 @@ function addCapitals(){
 var arcOrigin = [];
 var arcDestinations = [];
 var arcLinks = [];
+var activeLinks = [];
 
 function buildLinks(){
   $(worldcapitals.features).each(function(i, capital){
-    var caplat = capital.properties.LATITUDE;
-    var caplong = capital.properties.LONGITUDE;
+    // var caplat = capital.properties.LATITUDE;
+    // var caplong = capital.properties.LONGITUDE;
     if (capital.properties.ADM0_A3 == "USA"){      
       arcOrigin.push(capital);      
     } else {
@@ -114,7 +156,8 @@ function buildLinks(){
       ]
     });
   });
-  drawLink("NPL");  
+  $()
+  drawLinks();  
 }
 
 // --- Helper functions (for tweening the path)
@@ -141,24 +184,67 @@ var tweenDash = function tweenDash() {
     return function(t) { return interpolate(t); };
 };
 
-function drawLink(adm0){
+function drawLinks(){
   drawnLink = [];
+  
   $(arcLinks).each(function(i, link){
-    if (link.destination == adm0){
+    
+    if ($.inArray(link.destination, appealsCountries) != -1){
+      lineData = []; 
+      lineData.push(
+        {"x": projection(link.coordinates[0])[0], "y": projection(link.coordinates[0])[1]},
+        {"x": projection(link.coordinates[1])[0], "y": projection(link.coordinates[1])[1]}        
+        );
+
       drawnLink.push(link);
+      arcGroup.append("path")
+        .attr("d", line(lineData))
+        .style({
+          fill:'none',
+          stroke: '#0000ff',
+          'stroke-width': '1px'
+        })
+        .call(lineTransition);
     }
   });
-  arcGroup.selectAll(adm0)
-    .data(drawnLink)
-    .enter().append("path")
-    .attr('class', 'arc')
-    .attr('d', path)
-    .style({
-      fill:'none',
-      stroke: '#0000ff',
-      'stroke-width': '2px'
-    })
-    .call(lineTransition);
+   // arcGroup.selectAll("path")
+  //   .data(drawnLink)
+  //   .enter().append("path")
+  //   .attr('class', 'arc')
+  //   .attr('d', path)
+    
+  //   .style({
+  //     fill:'none',
+  //     stroke: '#0000ff',
+  //     'stroke-width': '1px'
+  //   })
+  //   .call(lineTransition);
+
+  // arcGroup.selectAll("path")
+  //   .data(lineData)
+  //   .enter().append("path")
+  //   .attr("d", line(linesData))
+  //   .style({
+  //     fill:'none',
+  //     stroke: '#0000ff',
+  //     'stroke-width': '1px'
+  //   })
+
+
+  // arcGroup.selectAll("line")
+  //   .data(drawnLink)
+  //   .enter().append("line")
+  //   .attr("x1", function(d){return projection(d.coordinates[0])[0]})
+  //   .attr("y1", function(d){return projection(d.coordinates[0])[1]})
+  //   .attr("x2", function(d){return projection(d.coordinates[1])[0]})
+  //   .attr("y2", function(d){return projection(d.coordinates[1])[1]})
+  //   .attr("transform", '')
+  //   .style({
+  //     fill:'none',
+  //     stroke: '#0000ff',
+  //     'stroke-width': '1px'
+  //   });
+
 }
 
 
