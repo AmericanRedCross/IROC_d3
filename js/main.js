6b675c30012c247c0bc99f6cef35b88f56c1bdbe
danbjoseph <danbjoseph@gmail.com>
2013-09-06 16:17:54 -0400

    end of day at ARC office

--- js/main.js
+++ js/main.js
@@ -2,28 +2,21 @@ var worldcountries = [];
 var worldcapitals = [];
 var appeals = [];
 var appealsCountries = [];
+var responses = [];
+var responsesCountries = [];
+var displayedAppeals = [];
 
+var windowWidth = $(window).width();
+var mapW = windowWidth * 0.70;
 
-var width = 960,
-    height = 960,
-    m = .25, // drag sensitivity
+var width = mapW,
+    height = 600,
     format = d3.format(".1f");
 
-// var projection = d3.geo.orthographic()
-//     .clipAngle(90)
-//     .rotate([-5, -5])
-//     .precision(.1)
-//     .translate([width / 2, height / 2])
-//     .scale(470);
-
-// var gilbert = d3.geo.gilbert(projection);
-
-// var path = d3.geo.path().projection(gilbert)
-//   .pointRadius(2);
 
 var projection = d3.geo.projection(d3.geo.hammer.raw(1.75, 2))
-    .rotate([-10, -45])
-    .scale(210);
+    .rotate([-30, 45])
+    .scale(200);
 
 var path = d3.geo.path()
     .projection(projection)
@@ -43,8 +36,33 @@ var svg = d3.select("#map1").append("svg")
   .attr("height", height);
 
 var countryGroup = svg.append('g').attr("id", "countries");
-var arcGroup = svg.append('g').attr("id", "arcs");
-var capitalGroup = svg.append('g').attr("id", "capitals");
+var responseGroup = svg.append('g').attr("id", "arcs");
+var appealGroup = svg.append('g').attr("id", "capitals");
+
+// --- Helper functions (for tweening the path)
+var lineTransition = function lineTransition(path) {
+    path.transition()
+        //NOTE: Change this number (in ms) to make lines draw faster or slower
+        .duration(5500)
+        .attrTween("stroke-dasharray", tweenDash)
+        .each("end", function(d,i) { 
+            ////Uncomment following line to re-transition
+            //d3.select(this).call(transition); 
+            
+            ////We might want to do stuff when the line reaches the target,
+            //doStuffWhenLineFinishes(d,i);
+        });
+};
+var tweenDash = function tweenDash() {
+    //This function is used to animate the dash-array property, which is a
+    //  nice hack that gives us animation along some arbitrary path (in this
+    //  case, makes it look like a line is being drawn from point A to B)
+    var len = this.getTotalLength(),
+        interpolate = d3.interpolateString("0," + len, len + "," + len);
+
+    return function(t) { return interpolate(t); };
+};
+
 
 
 function getcountrydata(){
@@ -73,12 +91,25 @@ function getappealdata(){
       timeout: 10000,
       success: function(json) {
         appeals = json;
-        $(appeals).each(function(i, appeal){
-          country = appeal.ADM0_A3;
-          if ($.inArray(country, appealsCountries) == -1){
-            appealsCountries.push(country);
-          }
-        });
+        
+        getresponsedata();
+      },
+      error: function(e) {
+          console.log(e);
+      }
+  });
+}
+
+function getresponsedata(){
+  $.ajax({
+      type: 'GET',
+      url: 'data/iroc_response.json',
+      contentType: 'application/json',
+      dataType: 'json',
+      timeout: 10000,
+      success: function(json) {
+        responses = json;
+        
         getcapitaldata();
       },
       error: function(e) {
@@ -96,7 +127,7 @@ function getcapitaldata(){
       timeout: 10000,
       success: function(json) {
         worldcapitals = json;
-        mapIt();
+        addCountries();
       },
       error: function(e) {
         console.log(e);
@@ -104,12 +135,7 @@ function getcapitaldata(){
   });
 }
 
-function mapIt(){
-  // svg.append("path")
-  //   .datum({type: "Sphere"})
-  //   .attr("class", "background")
-  //   .attr("d", path);
-
+function addCountries(){
  countryGroup.selectAll("path")
     .data(worldcountries.features)
     .enter().append("path")
@@ -117,29 +143,15 @@ function mapIt(){
     .attr('data-name', function(d){return d.properties.name;})        
     .attr('class', 'country')
     .attr("d", path);    
-  addCapitals();
-}
-
-function addCapitals(){
-  capitalGroup.selectAll("path")
-    .data(worldcapitals.features)
-    .enter().append("path")      
-    .attr('data-ADM0', function(d){return d.properties.ADM0_A3;})
-    .attr('class', 'capital')  
-    .attr("d", path);
   buildLinks();
 }
-
 
 var arcOrigin = [];
 var arcDestinations = [];
 var arcLinks = [];
-var activeLinks = [];
 
 function buildLinks(){
-  $(worldcapitals.features).each(function(i, capital){
-    // var caplat = capital.properties.LATITUDE;
-    // var caplong = capital.properties.LONGITUDE;
+  $(worldcapitals.features).each(function(i, capital){    
     if (capital.properties.ADM0_A3 == "USA"){      
       arcOrigin.push(capital);      
     } else {
@@ -157,47 +169,61 @@ function buildLinks(){
     });
   });
   $()
-  drawLinks();  
+  addAppeals(2011);  
 }
 
-// --- Helper functions (for tweening the path)
-var lineTransition = function lineTransition(path) {
-    path.transition()
-        //NOTE: Change this number (in ms) to make lines draw faster or slower
-        .duration(5500)
-        .attrTween("stroke-dasharray", tweenDash)
-        .each("end", function(d,i) { 
-            ////Uncomment following line to re-transition
-            //d3.select(this).call(transition); 
-            
-            ////We might want to do stuff when the line reaches the target,
-            //doStuffWhenLineFinishes(d,i);
-        });
-};
-var tweenDash = function tweenDash() {
-    //This function is used to animate the dash-array property, which is a
-    //  nice hack that gives us animation along some arbitrary path (in this
-    //  case, makes it look like a line is being drawn from point A to B)
-    var len = this.getTotalLength(),
-        interpolate = d3.interpolateString("0," + len, len + "," + len);
+function addAppeals(year){
+  $(appeals).each(function(i, appeal){
+    country = appeal.ADM0_A3;
+    var d = new Date(appeal.ST_DATE);
+    var appealYear = d.getFullYear();
+    if (appealYear == year){
+      if ($.inArray(country, appealsCountries) == -1){
+        appealsCountries.push(country);
+      }
+    }    
+  });
+  $(worldcapitals.features).each(function(i, capital){
+    if ($.inArray(capital.properties.ADM0_A3, appealsCountries) != -1){
+      displayedAppeals.push(capital);
+    }
+  });
+  appealGroup.selectAll("path")
+    .data(displayedAppeals)
+    .enter().append("path")      
+    .attr('data-ADM0', function(d){return d.properties.ADM0_A3;})
+    .attr('class', 'appeal')  
+    .attr("d", path);
+  addResponses(year);
+}
 
-    return function(t) { return interpolate(t); };
-};
 
-function drawLinks(){
+
+
+function addResponses(year){
+  $(responses).each(function(i, response){
+    country = response.ADM0_A3;
+    var d = new Date(response.Date);
+    var responseYear = d.getFullYear();
+    if (responseYear == year){
+      if ($.inArray(country, responsesCountries) == -1){
+        responsesCountries.push(country);
+      }  
+    }
+    
+  });
+
   drawnLink = [];
-  
   $(arcLinks).each(function(i, link){
     
-    if ($.inArray(link.destination, appealsCountries) != -1){
+    if ($.inArray(link.destination, responsesCountries) != -1){
       lineData = []; 
       lineData.push(
         {"x": projection(link.coordinates[0])[0], "y": projection(link.coordinates[0])[1]},
         {"x": projection(link.coordinates[1])[0], "y": projection(link.coordinates[1])[1]}        
         );
-
       drawnLink.push(link);
-      arcGroup.append("path")
+      appealGroup.append("path")
         .attr("d", line(lineData))
         .style({
           fill:'none',
@@ -207,43 +233,6 @@ function drawLinks(){
         .call(lineTransition);
     }
   });
-   // arcGroup.selectAll("path")
-  //   .data(drawnLink)
-  //   .enter().append("path")
-  //   .attr('class', 'arc')
-  //   .attr('d', path)
-    
-  //   .style({
-  //     fill:'none',
-  //     stroke: '#0000ff',
-  //     'stroke-width': '1px'
-  //   })
-  //   .call(lineTransition);
-
-  // arcGroup.selectAll("path")
-  //   .data(lineData)
-  //   .enter().append("path")
-  //   .attr("d", line(linesData))
-  //   .style({
-  //     fill:'none',
-  //     stroke: '#0000ff',
-  //     'stroke-width': '1px'
-  //   })
-
-
-  // arcGroup.selectAll("line")
-  //   .data(drawnLink)
-  //   .enter().append("line")
-  //   .attr("x1", function(d){return projection(d.coordinates[0])[0]})
-  //   .attr("y1", function(d){return projection(d.coordinates[0])[1]})
-  //   .attr("x2", function(d){return projection(d.coordinates[1])[0]})
-  //   .attr("y2", function(d){return projection(d.coordinates[1])[1]})
-  //   .attr("transform", '')
-  //   .style({
-  //     fill:'none',
-  //     stroke: '#0000ff',
-  //     'stroke-width': '1px'
-  //   });
 
 }
 
